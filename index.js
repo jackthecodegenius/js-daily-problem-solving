2fb1eb6ad9f4d0015c131d8ed0a8d39d12b20915
jackthecodegenius <jackcockerill2222@gmail.com>
2026-07-22 16:21:51 +1000

    adding nested loops functionality

--- index.js
+++ index.js
@@ -595,13 +595,37 @@
 
 // console.log(getEvens([8, 9, 10, 14, 15, 16]));
 
-function findMax(nums) {
-  let max = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    if (max < nums[i]) {
-      max = nums[i];
-    }
-  }
-  return max;
-}
-console.log(findMax([30, 5, 10, 20]));
+// function findMax(nums) {
+//   let max = nums[1];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (max < nums[i]) {
+//       max = nums[i];
+//     }
+//   }
+//   return max;
+// }
+// console.log(findMax([30, 5, 10, 20]));
+
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
+// console.log(twoSum([2, 7, 11, 15], 18));
+
+// function findMatchingWords(words, letter) {
+//   let output = [];
+//   for (let i = 0; i < words.length; i++) {
+//     if (words[i][0] === letter) {
+//       output.push(words[i]);
+//     }
+//   }
+//   return output;
+// }
+
+// console.log(findMatchingWords(["apple", "banana", "avocado", "cherry"], "a"));
